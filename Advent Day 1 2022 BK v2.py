54bd020111ea2b96f1599ea537c7facc804af160
BrendanKenalty <119376677+BrendanKenalty@users.noreply.github.com>
2022-12-01 17:37:18 -0500

    Update Advent Day 1 2022 BK v2.py

--- Advent Day 1 2022 BK v2.py
+++ Advent Day 1 2022 BK v2.py
@@ -1,4 +1,5 @@
 
+from collections import Counter
 import numpy as np
 
 #Wooks
@@ -31,6 +32,21 @@ for j in raw:
 print(calTracker,"------CalT------")
 
 print('Part 1 Answer: ')
-print('The elf with the most calories is elf number: ' + str(max(elfTracker)))
+print('The elf with the most calories is elf number: ' + str(max(elfTracker))) 
+# change INT back to string for printing
 print('They have ' + str(max(elfTracker.values())) + ' calories')
 
+# PArt 2
+
+#c = Counter(elfTracker)
+#top3 = [j[1] for j in c.most_common(3)]
+
+#print('Part 2 Answer: ')
+#print('The total calories carried by the top 3 elves is: ' + str(sum(top3)))
+
+Elfvalues = list(elfTracker.values())
+Elfvalues.sort()
+
+print(Elfvalues[-3:])
+
+#print(totalCals[-3:], "Sum top 3", np.sum(totalCals[-3:]))
